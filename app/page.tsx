4b98c730dad6bdab468c15194d148358780e7e3a
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-03-16 23:22:38 +0700

    v1.0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center">
+        <h1 className="text-xl font-bold mb-6">
+          Welcome to my Next.ts Portfolio
+        </h1>
+        <div className="flex flex-col space-y-4">
+          <Link href="/login">
+            <p className="bg-white border border-blue-500 text-blue-600 font-bold py-2 px-4 rounded hover:bg-blue-400 hover:text-white">
+              Login
+            </p>
+          </Link>
+          <Link href="/navbar">
+            <p className="bg-white border border-blue-500 text-blue-600 font-bold py-2 px-4 rounded hover:bg-blue-400 hover:text-white">
+              Navbar
+            </p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
