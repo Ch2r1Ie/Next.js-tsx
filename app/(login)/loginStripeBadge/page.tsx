856d3a908e150fde60527388938c74bf5c289da8
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-03-17 13:58:25 +0700

    v1.0.5

--- app/(login)/loginStripeBadge/page.tsx
+++ app/(login)/loginStripeBadge/page.tsx
@@ -25,7 +25,7 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex items-center justify-center min-h-screen min-w-[300px] bg-gray-100 dark:bg-gray-900 overflow-auto">
       <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full max-w-md">
         {isSuccessful ? (
           <p className="text-green-500 text-center text-lg font-semibold">
