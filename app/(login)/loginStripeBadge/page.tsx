dca57178957fcfbba85f8af65d83f986048f2084
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-03-17 14:21:06 +0700

    v1.0.6

--- app/(login)/loginStripeBadge/page.tsx
+++ app/(login)/loginStripeBadge/page.tsx
@@ -25,7 +25,14 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen min-w-[300px] bg-gray-100 dark:bg-gray-900 overflow-auto">
+    <div
+      className="flex items-center justify-center min-h-screen min-w-[300px] bg-gray-100 dark:bg-gray-900 overflow-auto animate-bg-scroll"
+      style={{
+        backgroundImage: "url('/assets/sea-background.jpg')",
+        backgroundSize: "cover",
+        backgroundRepeat: "repeat-y",
+      }}
+    >
       <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full max-w-md">
         {isSuccessful ? (
           <p className="text-green-500 text-center text-lg font-semibold">
